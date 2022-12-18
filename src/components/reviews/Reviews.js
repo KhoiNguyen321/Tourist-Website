4a51e223be86a5564f41bc1c4e65e5a4699eaeff
khai.vuong@watasolutions.com <khai.vuong@watasolutions.com>
2022-12-18 14:42:49 +0700

    1. Access Reviews Array from Context API Store 2. Loop Reviews Array 3. Style Review Component 4. Rating Function 5. Load Lazy Images

---
+++ src/components/reviews/Reviews.js
@@ -0,0 +1,24 @@
+import React, { useState, useContext } from 'react';
+import SharedContext from '../../context/SharedContext';
+import ReviewBody from './ReviewBody';
+const Reviews = () => {
+  const [heading] = useState('What our customers are saying?');
+  const {
+    sharedData: { reviews },
+  } = useContext(SharedContext);
+  return (
+    <div className='reviews'>
+      <div className='container'>
+        <h2 className='heading'>{heading}</h2>
+        <div className='row ml-minus-15 mr-minus-15'>
+          {reviews.length > 0 &&
+            reviews.map((review, index) => (
+              <ReviewBody review={review} myKey={index} />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Reviews;
